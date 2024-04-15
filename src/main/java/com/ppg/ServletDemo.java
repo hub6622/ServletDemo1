857814e6e42c9ed23d584599376c3b54e1daa52b
PPG <16333@PPG>
2024-04-15 11:43:00 +0800

    add ServletDemo cookies_used

---
+++ src/main/java/com/ppg/ServletDemo.java
@@ -0,0 +1,32 @@
+package com.ppg;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo1")
+public class ServletDemo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("收到请求");
+        String myheader = req.getHeader("myheader");
+        Cookie cookie = new Cookie("name","lhh");
+        resp.addCookie(cookie);
+        String str = req.getParameter("hb3");
+        String str2 = req.getParameter("name");
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(myheader);
+        resp.getWriter().write("hello!!!!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
